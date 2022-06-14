b5215520c6b253d693b78dbafce9a9c179893f0f
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-06-14 13:07:58 +0530

    Added solution - LeetHub

---
+++ Move all zeros to the front of the linked list - GFG/move-all-zeros-to-the-front-of-the-linked-list.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node **head_ref, int new_data) {
+    struct Node *new_node = new Node(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void moveZeroes(struct Node **head);
+
+// function to displaying nodes
+void display(struct Node *head) {
+    while (head != NULL) {
+	    cout << head->data << " ";
+	    head = head->next;
+    }
+    cout <<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int q;
+        vector<int> v;
+        struct Node *Head=NULL;
+        for(int i=0;i<n;i++)
+        {
+            cin>>q;
+            v.push_back(q);
+        }
+        reverse(v.begin(),v.end());
+        for(auto q:v)
+            push(&Head,q);
+        moveZeroes(&Head);
+        display(Head);
+
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+/* Linked List Node structure
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+// Move Zeros to the front of the list
+void moveZeroes(struct Node **head)
+{
+    //Your code here
+    struct Node* p= (*head);
+    struct Node* pre=NULL;
+    struct Node* ne= p->next;
+    while(p->next!=NULL && p!=NULL)
+    {
+        
+        if(p->data==0 && p!=(*head))
+        {
+            struct Node* r= p;
+            p=p->next;
+            pre->next=ne;
+            delete(r);
+            ne=p->next;
+            struct Node *new_node = new Node(0);
+            new_node->next = (*head);
+            (*head) = new_node;
+        }
+        else{
+            pre=p;
+            p=p->next;
+            ne=p->next;
+        }
+        
+        if(p->next==NULL && p->data==0)
+        {
+            struct Node* r= p;
+            p=pre;
+            pre->next=NULL;
+            
+            delete(r);
+            struct Node *new_node = new Node(0);
+            new_node->next = (*head);
+            (*head) = new_node;
+        }
+        
+    }
+    
+}
