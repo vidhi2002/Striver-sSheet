f451dc1bf8fab8df54ca8588bbd182cdd8ea0d1d
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-07-14 22:43:01 +0530

    Time: 46 ms (34.66%), Space: 28.4 MB (22.64%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int calc(vector<string>& words,map<char,int>&m,vector<int>& score,int i){ 
-        int maxi=0;
+int sol(vector<string>& words, map<char,int> mp, vector<int>& score,int i)
+    {
         if(i==words.size())
             return 0;
-        map<char,int>m1=m;//Creating a duplicate in case the given word does not satisfy our answer
-        int c=0;//Store the score
-        for(int j=0;j<words[i].length();j++){
-            if(m1.find(words[i][j])==m1.end()||m1[words[i][j]]==0){//If frequency of the letter reduces to zero or that letter is not present in our hashMap            
-                c=-1;
-                break;
+        int sno=sol(words,mp,score,i+1);
+        int sword=0;
+        string word=words[i];
+        bool flag=true;
+        for(int j=0;j<word.length();j++)
+        {
+            //char ch=word[j];
+            if(mp.find(word[j])==mp.end() || mp[word[j]]==0){
+                flag=false;
             }
-            c+=score[words[i][j]-'a'];
-            m1[words[i][j]]--;
-        }        
-        if(c!=-1)           
-          maxi=c+(calc(words,m1,score,i+1));//We are considering the current word and adding it to our answer
-        return max(maxi,calc(words,m,score,i+1));//FInding tha maximum between when we consider the current word and when we DONT consider current word
+            mp[word[j]]--;
+            sword+=score[word[j]-'a'];
+        }
+        int syes=0;
+        if(flag)
+            syes=sword+sol(words,mp,score,i+1);
         
-}
+        return max(sno,syes);
+    }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        map<char,int>m;       
-        for(auto i:letters)
-            m[i]++;
-        return calc(words,m,score,0);   
+        map<char,int> mp;
+        for(int i=0;i<letters.size();i++)
+            mp[letters[i]]++;
+        return sol(words,mp,score,0);
     }
 };
