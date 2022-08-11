3ce548a4277b6fe5e2a9869327f0cd81631abd2e
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-08-11 15:39:50 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (87.97%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        long long alice=0,bob=0;
+        int i=0,j=piles.size()-1,check=0;
+        while(i<j)
+        {
+            if(check==0)
+            {
+                if(piles[i]>piles[j])
+                {
+                    alice+=piles[i];
+                    i++;
+                }
+                else
+                {
+                    alice+=piles[j];
+                    j--;
+                }
+                check=1;
+            }
+            else
+            {
+                if(piles[i]>piles[j])
+                {
+                    bob+=piles[j];
+                    j--;
+                }
+                else
+                {
+                    bob+=piles[i];
+                    i++;
+                }
+                check=0;
+            }
+        }
+        if(alice>bob)
+            return true;
+        return false;
     }
 };
