9b5bdc3c871ccc59239397f260555c74656cf558
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-08-27 12:32:51 +0530

    Time: 28 ms (21.64%), Space: 12.5 MB (86.07%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,21 +1,52 @@
-class Solution {
-public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        vector<string> result;
-        map<string, int> wordCounts;
-        int maxCount = 0;
-        
-        for (string &s: words) maxCount = max(++wordCounts[s], maxCount);
-        
-        while (maxCount > 0 && k > 0) {
-            for (auto it = wordCounts.begin(); it != wordCounts.end(); ++it) {
-                if (it->second == maxCount) {
-                    result.push_back(it->first);
-                    if (--k == 0) break;
-                }
-            }
-            --maxCount;
+class Vaibhav
+{
+      public:
+      string word;
+      int freq;
+      Vaibhav(string word,int freq)
+      {
+          this->word=word;
+          this->freq=freq;
+      }
+};
+class Mycompare
+{
+    public:
+    bool operator ()(Vaibhav item1,Vaibhav item2)
+    {
+        if(item1.freq!=item2.freq)
+        {
+            return item1.freq<item2.freq;
         }
-        return result;
+        else
+        {
+            return item1.word>item2.word;
+        }
+    }
+};
+
+class Solution {
+    public:
+    vector<string> topKFrequent(vector<string>& words, int k) 
+    {
+        map<string,int>mymap;
+        for(auto word:words)
+            mymap[word]++;
+        priority_queue<Vaibhav,vector<Vaibhav>,Mycompare>highF;
+        for(auto each:mymap)
+        {
+            string word=each.first;
+            int freq=each.second;
+            Vaibhav mypair(word,freq);
+            highF.push(mypair);
+        }
+        vector<string>ans;
+        while(k--)
+        {
+            Vaibhav topMost=highF.top();
+            highF.pop();
+            ans.push_back(topMost.word);
+        }
+        return ans;
     }
 };
