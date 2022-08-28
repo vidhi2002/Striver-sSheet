e916b3c32ccdd6be49a9651e9066f4ad392386e6
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-08-28 09:54:00 +0530

    Time: 5 ms (17.91%), Space: 6.6 MB (18.96%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
 bool wordPattern(string pattern, string s) {
-vector<string> str;
-string temp="";
-for(int i=0;i<s.size();i++)
-{
-if(s[i]!=' ')
-{
-temp+=s[i];
-}
-else
-{
-str.push_back(temp);
-temp="";
-}
-}
-str.push_back(temp);
-if(pattern.size()!=str.size())
-return false;
-unordered_map<char,string>mp;
-unordered_set<string> st;
-for(int i=0;i<str.size();i++)
-{
-if(mp.find(pattern[i])!=mp.end())
-{
-if(mp[pattern[i]]!=str[i])
-return false;
-}
-else
-{
-if(st.count(str[i])>0)
-return false;
-mp[pattern[i]]=str[i];
-st.insert(str[i]);
-}
-}
-return true;
+    vector<string> str;
+    stringstream v(s);
+    string word;
+    while(v>>word)
+        str.push_back(word);
+    if(pattern.size()!=str.size())
+        return false;
+    unordered_map<char,string> mp;
+    unordered_set<string> st;
+    for(int i=0;i<str.size();i++)
+    {
+        if(mp.find(pattern[i])!=mp.end())
+        {
+            if(mp[pattern[i]]!=str[i])
+                return false;
+        }
+        else
+        {
+            if(st.count(str[i])>0)
+                return false;
+            mp[pattern[i]]=str[i];
+            st.insert(str[i]);
+        }
+    }
+    return true;
 }
 };
