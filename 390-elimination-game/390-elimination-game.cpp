a05b677b00d741af8bf35c393e5518cd8559447b
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-09-05 07:05:31 +0530

    Time: 7 ms (62.97%), Space: 6 MB (26.01%) - LeetHub

--- 390-elimination-game/390-elimination-game.cpp
+++ 390-elimination-game/390-elimination-game.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int _lastRemaining(int n, int start){
+    int find(int n, int start){
         if(n==1) return 1;
-        if(start == 0){//0 means we start from begining
-            return 2*_lastRemaining(n/2,1-start);
+        if(start == 0){
+            return 2*find(n/2,1-start);
         }
         else{
-            if(n&1){//if last digit is odd, then all odd numbers are deleted
-                return 2*_lastRemaining(n/2,1-start); 
-            }// all odd numbers are deleted, the make the sequence 1 to n by adding 1 to every element and then dividing by 2.
-            return 2*_lastRemaining(n/2,1-start)-1;
+            if(n&1){
+                return 2*find(n/2,1-start); 
+            }
+            return 2*find(n/2,1-start)-1;
         }
     }
     int lastRemaining(int n) {
-        return _lastRemaining(n,0);
+        return find(n,0);
     }
 };
