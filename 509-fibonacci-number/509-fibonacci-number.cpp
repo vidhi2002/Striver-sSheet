de4080c157801a2ce518f9907283a9c840ce9446
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-09-17 17:20:19 +0530

    Time: 18 ms (30.99%), Space: 5.9 MB (78.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int fib(int n) {
+    int find(int n)
+    {
         if(n==0)
             return 0;
-        if(n==1 || n==2)
+        if(n==1)
             return 1;
-        int prev1=1,prev2=1;
-        for(int i=3;i<=n;i++)
-        {
-            int cur=prev1+prev2;
-            prev1=prev2;
-            prev2=cur;
-        }
-        return prev2;
-        
-        
+        return find(n-1)+find(n-2);
+    }
+    int fib(int n) {
+        return find(n);
     }
 };
