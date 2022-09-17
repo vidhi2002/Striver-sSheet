f80dd33a1f965c20a46f734b86bd08198b5ebd32
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-09-17 17:22:09 +0530

    Time: 3 ms (63.31%), Space: 6 MB (17.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,17 @@
+vector<int> t(31,-1);
 class Solution {
+    
 public:
+    
     int find(int n)
     {
         if(n==0)
             return 0;
         if(n==1)
             return 1;
-        return find(n-1)+find(n-2);
+        if(t[n]!=-1)
+            return t[n];
+        return t[n]=find(n-1)+find(n-2);
     }
     int fib(int n) {
         return find(n);
