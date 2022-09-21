40926c6e7f2c3caaea13e69bb0be381c3ddf0d5d
vidhi2002 <68370343+vidhi2002@users.noreply.github.com>
2022-09-21 09:03:14 +0530

    Time: 181 ms (5.08%), Space: 29.3 MB (6.70%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    long long summ(vector<int>& nums,int n)
-    {
-        long long ans=0;
-        for(int i=0;i<n;i++)
-            ans+=nums[i];
-        return ans;
-    }
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int n=nums.size();
-        if(target>summ(nums,n))
-            return 0;
-        if(target==summ(nums,n))
-            return n;
-        int i=0,j=0,cur=0,len=INT_MAX;
-        while(i<n && j<n)
-        {
-            cur+=nums[j];
-            while(cur>=target)
-            {
-                len=min(len,j-i+1);
-                cur-=nums[i];
-                i++;
-            }
-            j++;
+    int minSubArrayLen(int s, vector<int>& nums) {
+         int n = nums.size();
+    if (n == 0)
+        return 0;
+    int ans = INT_MAX;
+    vector<int> sums(n + 1, 0); //size = n+1 for easier calculations
+    //sums[0]=0 : Meaning that it is the sum of first 0 elements
+    //sums[1]=A[0] : Sum of first 1 elements
+    //ans so on...
+    for (int i = 1; i <= n; i++)
+        sums[i] = sums[i - 1] + nums[i - 1];
+    for (int i = 1; i <= n; i++) {
+        int to_find = s + sums[i - 1];
+        auto bound = lower_bound(sums.begin(), sums.end(), to_find);
+        if (bound != sums.end()) {
+            ans = min(ans, static_cast<int>(bound - (sums.begin() + i - 1)));
         }
-            
-        return len;
+    }
+    return (ans != INT_MAX) ? ans : 0;
     }
 };
